5cf8ed257bc86fdc884dfdead0d0717f7ba86314
princemaxi <princemaxwellugochukwu@gmail.com>
2023-08-01 23:24:28 +0100

    Create addition function

--- script.js
+++ script.js
@@ -11,7 +11,9 @@ console.log(addition()); */
 //Create variables to accept first and second user input
 let a = parseInt(prompt("Enter first number: ")) ;
 let b = parseInt(prompt("Enter second number: ")) ;
+
 //Create an addition function
+function addition() {}
 // The addition function will return the addition for input a and b
 // invoke addition function to run the calculator
 
