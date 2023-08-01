4953030e6b6350cdcf06cc15d60dabdb70d17995
princemaxi <princemaxwellugochukwu@gmail.com>
2023-08-01 23:14:35 +0100

    Create pseudocode for the calculator

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-let a = parseInt(prompt("Insert first number"));
+/*let a = parseInt(prompt("Insert first number"));
 let b = parseInt(prompt("Insert second number"));
 
 function addition() {
     return a + b;
 }
 
-console.log(addition());
+console.log(addition()); */
+
+
+//Create variables to accept first and second user input
+//Create an addition function
+// The addition function will return the addition for input a and b
+// invoke addition function to run the calculator
+
